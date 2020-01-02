6fdc9b273b5c0ad60a1382b3c2efd15d51e5d124
Geoffrey Clements <baldmountain@gmail.com>
2020-01-02 11:02:31 -0500

    Make sure stuff is installed

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 set -ev
 cd firmware
+mix local.rebar --force
+mix local.hex --force
 mix deps.get
 mix test
 cd ../ui
+mix local.rebar --force
+mix local.hex --force
 mix deps.get
 mix test
