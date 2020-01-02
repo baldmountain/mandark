c6f7ad172fe8ee6e996f8aa40f28d3f10c5aa2de
Geoffrey Clements <baldmountain@gmail.com>
2020-01-02 15:27:23 -0500

    Build more before tests

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 set -ev
-cd firmware
+cd ui
 mix local.rebar --force
 mix local.hex --force
 mix deps.get
+cd assets
+npm install
+npm rum deploy
+cd ..
+mix phx.digest
 mix test
-cd ../ui
+cd ../firmware
 mix local.rebar --force
 mix local.hex --force
+mix archive.install --force hex nerves_bootstrap
 mix deps.get
 mix test
