fbba5e75615b0bac68c617cdd83f47d2f228e3a5
Geoffrey Clements <baldmountain@gmail.com>
2020-01-25 00:00:29 -0500

    Fix temp

--- ui/lib/ui_web/views/page_view.ex
+++ ui/lib/ui_web/views/page_view.ex
@@ -4,7 +4,7 @@ defmodule UiWeb.PageView do
   def get_measurements() do
       vals = GenServer.call(Firmware.Sensors, :get_sensor_data)
       %{
-        temperature: vals.temperature * 9>0 / 5.0 + 32.0,
+        temperature: vals.temperature * 9.0 / 5.0 + 32.0,
         humidity: vals.humidity,
         preasure: vals.pressure * 7.50062
       }
