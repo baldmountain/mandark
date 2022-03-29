1390a4eff4a21d0f004d207bde88d6a2b031c4e5
Geoffrey Clements <baldmountain@gmail.com>
2022-03-29 19:38:06 -0400

    Update libraries
    Make target specific config files and a .envrc file to choose between
    them.

---
+++ firmware/config/rpi0.exs
@@ -0,0 +1,7 @@
+use Mix.Config
+
+config :firmware,
+  sensor: %{
+    sensor: :dht22,
+    gpio: 6
+  }
--- firmware/config/target.exs
+++ firmware/config/target.exs
@@ -84,4 +84,4 @@ config :tzdata, :autoupdate, :disabled
 # of this file so it overrides the configuration defined above.
 # Uncomment to use target specific configurations
 
-# import_config "#{Mix.target()}.exs"
+import_config "#{Mix.target()}.exs"
